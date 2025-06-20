7427e2dd7a5866cc4d26381939ff509febbcb705
Pedro Simoes <176055646+404PeterSimoes@users.noreply.github.com>
2025-06-20 11:57:19 +0100

    Provável finalização do programa 2, comentários feitos

--- Pedro Simoes - Trabalho 2 - Tratamento Erros Excecao Personalizada.py
+++ Pedro Simoes - Trabalho 2 - Tratamento Erros Excecao Personalizada.py
@@ -15,131 +15,161 @@ class ContaBancaria:
 
     def depositar(self, valor):
 
-        if valor < 0:
+        # Verificar se o valor introduzido é igual ou menor que 0
+        if valor <= 0:
             raise ValorInvalidoError
 
         self.saldo += valor
 
     def levantar(self, valor):
 
+        # Verificar se o valor introduzido é igual ou menor que 0
+        if valor <= 0:
+            raise ValorInvalidoError
+
+        # Verificar se o saldo atual é inferior ao que se pretende levantar
         if self.saldo < valor:
             raise SaldoInsuficienteError
 
         self.saldo -= valor
 
+# Limpar o terminal
 def limpar():
     if os.name == 'nt':
         os.system('cls')
     else:
         os.system('clear')
 
+# Lista com as Contas Bancárias
 contas = [
-    conta_manuel := ContaBancaria('Manuel Josefandro', 545),
-    conta_joaquina := ContaBancaria('Joaquina Garcia', 734)
+    ContaBancaria('Manuel Josefandro', 545),
+    ContaBancaria('Joaquina Garcia', 734),
+    ContaBancaria('Pedro Miguel', 1000)
 ]
 
+# Ciclo infinito
+while True:
+    limpar()
 
-limpar()
-print('   Menu de Contas:\n---------------------')
-for i in range(0, len(contas)):
-    print(f'{i + 1} - {contas[i].titular}')
-
-verificacao = False
-while not verificacao:
-    try:
-        n = int(input('\nIntroduza o número de uma conta: '))
-        n -= 1
-
-    # Caso o valor introduzido não seja um inteiro
-    except:
-        print('Tenta novamente.')
-
-    # Se for inteiro
-    else:
-        limpar()
+    verificacao = False
+    while not verificacao:
         try:
-            contaselecionada = contas[n]
-        
-        # Caso o valor introduzido não seja um índice válido da lista de contas
-        except IndexError:
-            print('O número de conta introduzido não existe')
 
-        # Se for um índice válido, sair do ciclo
+            # Imprimir o menu de contas
+
+            print('Menu:\n------------------------')
+            for i in range(0, len(contas)):
+                print(f'{i + 1} - {contas[i].titular}')
+
+            print('------------------------\n0 - Sair')
+
+            n = int(input('\nIntroduza uma opção: '))
+
+            # Terminar o programa
+            if n == 0:
+                exit()
+
+            n -= 1
+                
+        # Caso o valor introduzido não seja um inteiro
+        except ValueError:
+            print('Tente novamente.\n')
+
+        # Se for inteiro
+        else:
+            try:
+
+                if n < 0:
+                    raise IndexError
+
+                # Forçar verificação de índice válido
+                contaselecionada = contas[n]
+            
+            # Caso o valor introduzido não seja um índice válido da lista de contas
+            except IndexError:
+                print('O número de conta introduzido não existe.\n')
+
+            # Se for um índice válido, sair do ciclo
+            else:
+                limpar()
+                verificacao = True
+
+    # Imprimir atributos da conta selecionada
+    print(f'Conta selecionada:\n\n'
+        f'{n + 1} - {contaselecionada.titular}\n'
+        f'Saldo - {contaselecionada.saldo}€\n')
+
+    verificacao = False
+    while not verificacao:
+        try:
+            acao = int(input('Que ação pretende realizar?'
+                        '\n1 - Depositar'
+                        '\n2 - Levantar\n'))
+            
+            if not (acao == 1 or acao == 2):
+                raise ValueError
+            
+        # Caso o valor introduzido não seja um inteiro
+        except ValueError:
+            print('Tente novamente.\n')
+
+        # Se for inteiro, sair do ciclo
         else:
             verificacao = True
 
-print(f'Conta selecionada:\n\n'
-      f'{n} - {contaselecionada.titular}\n'
-      f'Saldo - {contaselecionada.saldo}€')
 
-verificacao = False
-while not verificacao:
-    try:
-        acao = int(input('\nQue ação pretende realizar?'
-                        '\n1 - Depositar'
-                        '\n2 - Levantar\n'))
-        
-        if not (acao == 1 or acao == 2):
-            raise
-        
-    # Caso o valor introduzido não seja um inteiro
-    except:
-        print('Tenta novamente.')
+    verificacao = False
 
-    # Se for inteiro, sair do ciclo
-    else:
-        verificacao = True
+    # DEPOSITAR
+    if acao == 1:
 
+        while not verificacao:
+            try:
+                valor = int(input('\nQual o valor que pretende depositar?\n'))
 
-verificacao = False
-while not verificacao:
-    try:
-        if acao == 1:
-            valor = int(input(('Qual o valor que pretende depositar?\n')))
+                contaselecionada.depositar(valor)
+            
+            # Caso o valor introduzido seja inferior a 0
+            except ValorInvalidoError:
+                print('Não é possível depositar valores iguais ou inferiores a 0.')
 
-        # adicionar aqui?
+            # Caso o valor introduzido não seja um inteiro
+            except ValueError:
+                print('Tente novamente.')
 
-        elif acao == 2:
-            valor = int(input(('Qual o valor que pretende levantar?\n')))
+            # Se houver sucesso na operação
+            else:
+                print(f'\nValor de {valor}€ depositado na conta de nome {contaselecionada.titular}!\n'
+                    f'Novo Saldo - {contaselecionada.saldo}€\n')
+                
+                os.system("pause")
+                verificacao = True
 
-    except:
-        print('Tenta novamente.')
+    # LEVANTAR
+    elif acao == 2:
 
-    else:
-        verificacao = True
+        while not verificacao:
+            try:
+                valor = int(input('\nQual o valor que pretende levantar?\n'))
 
+                contaselecionada.levantar(valor)
 
-verificacao = False
+            # Caso o valor introduzido seja igual ou inferior a 0
+            except ValorInvalidoError:
+                print('Não é possível levantar valores iguais ou inferiores a 0.')
 
-if acao == 1:
+            # Verificar se há saldo suficiente para fazer o levantamento
+            except SaldoInsuficienteError:
+                print('Não possui saldo suficiente para esse levantamento.')
 
-    while not verificacao:
-        try:
-            contaselecionada.depositar(valor)
-        
-        except SaldoInsuficienteError:
-            print('O valor que pretende depositar não é válido.')
+            # Caso o valor intruduzido não seja um inteiro
+            except ValueError:
+                print('Tente novamente.')
 
-        except:
-            print('Tenta novamente.')
-
-        else:
-            print(f'Valor de {valor}€ depositado na conta de nome {contaselecionada.titular}!\n'
-                  f'Novo Saldo - {contaselecionada.saldo}€')
-
-elif acao == 2:
-
-    while not verificacao:
-        try:
-            contaselecionada.levantar(valor)
-
-
-        except ValorInvalidoError:
-            print('Não possui saldo suficiente para esse levantamento.')
-
-        except:
-            print('Tenta novamente.')
-
-        else:
-            print(f'Valor de {valor}€ depositado na conta de nome {contaselecionada.titular}!\n'
-                f'Novo Saldo - {contaselecionada.saldo}€')
+            # Se houver sucesso na operação
+            else:
+                print(f'\nValor de {valor}€ levantado da conta de nome {contaselecionada.titular}!\n'
+                    f'Novo Saldo - {contaselecionada.saldo}€\n')
+                
+                os.system("pause")
+                verificacao = True
